526ae0cb69ffc58f4752d4ef6dda9251d58c4ca1
super-neojos <super-neojos@users.noreply.github.com>
2026-04-14 09:51:48 +0800

    fix: 点击扩展图标时优先选中当前窗口的tab

--- ext/src/background.js
+++ ext/src/background.js
@@ -1,6 +1,8 @@
 chrome.action.onClicked.addListener(async () => {
     const url = chrome.runtime.getURL('popup.html');
-    const [tab] = await chrome.tabs.query({ url });
+    // 优先在当前窗口查找，再找其他窗口
+    const [tab] = await chrome.tabs.query({ url, currentWindow: true })
+        || await chrome.tabs.query({ url });
     if (tab && tab.id) {
         await chrome.tabs.update(tab.id, { active: true });
     } else {
